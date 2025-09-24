00c3b07a79f401ac386eec235b27608bf88990a9
charandeep singh <N01748813@humber.ca>
2025-09-23 20:11:28 -0400

    updates

---
+++ week1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let fname = 'Charry';
+        console.log(fname);
+        Document.write("Write back, " + fname + "<br>" );
+    </script>
+    <?php
+    $fname = "Charry";
+    echo "Welcome back, ". $fname;
+    echo "<h1 style=\"\">" . $fname . "</h1>"
+    ?>
+</body>
+</html>
